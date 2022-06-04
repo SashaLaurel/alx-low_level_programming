8511919cee4bdc900f84a7486cc551a39382cd92
sashalaurel <enquiries@sashalaurel.net>
2022-06-03 17:55:38 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+ 
